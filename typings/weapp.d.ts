6a26bc454a8084982823e905de8ffa7a4c2468e6
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-14 17:31:26 +0800

    feat: 增加分享类型

--- typings/weapp.d.ts
+++ typings/weapp.d.ts
@@ -1,3 +1,4 @@
 declare namespace IMippWe {
   interface IPageLifetime extends Partial<WechatMiniprogram.Page.ILifetime> {}
+  interface ICustomShareContent extends Partial<WechatMiniprogram.Page.ICustomShareContent> {}
 }
