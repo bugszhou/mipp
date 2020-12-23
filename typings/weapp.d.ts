f26c6cfec70e225e25076587c69566323e1c6865
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-23 14:34:40 +0800

    feat: 修改注释

--- typings/weapp.d.ts
+++ typings/weapp.d.ts
@@ -1,10 +1,10 @@
 declare namespace IMippWe {
   /**
-   * 页面生命周期方法
+   * Page页面生命周期方法
    */
   interface IPageLifetime extends Partial<WechatMiniprogram.Page.ILifetime> {}
   /**
-   * 页面分享返回值类型
+   * Page页面分享返回值类型
    */
   interface ICustomShareContent extends Partial<WechatMiniprogram.Page.ICustomShareContent> {}
 }
