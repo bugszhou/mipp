75f5bed7a7379def64d1491755cb54e7a8b3f7da
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-28 18:23:19 +0800

    feat: 增加Methods typings

--- typings/weapp/Component.d.ts
+++ typings/weapp/Component.d.ts
@@ -3,15 +3,30 @@
  *
  * 1. 使用该组件的页面的生命周期方法声明(IMippWeComponent.IPageLifetime)
  *
- * 2. 生命周期函数形参类型声明
+ * 2. 组件生命周期方法声明
  *
- * 3. 分享返回值声明(ICustomShareContent)
- *
- * 4. 返回值类型
  */
 declare namespace IMippWeComponent {
   /**
-   * 生命周期方法:
+   * Component生命周期方法：
+   *
+   * created(): void
+   *
+   * attached(): void
+   *
+   * ready(): void
+   *
+   * moved(): void
+   *
+   * detached(): void
+   *
+   * error(err: Error): void
+   *
+   */
+  interface ILifetimes extends Partial<WechatMiniprogram.Component.Lifetimes> {}
+
+  /**
+   * Page生命周期方法:
    *
    * onShow(): void
    *
@@ -19,7 +34,34 @@ declare namespace IMippWeComponent {
    *
    * onResize(options: IMippWePage.IResizeOption): void
    */
-  interface IPageLifetime extends Partial<WechatMiniprogram.Component.PageLifetimes> {}
+  interface IPageLifetimes<This> {
+    /** 页面生命周期回调—监听页面显示
+     *
+     * 页面显示/切入前台时触发。
+     */
+    show(this: This): void;
+    /** 页面生命周期回调—监听页面隐藏
+     *
+     * 页面隐藏/切入后台时触发。 如 `navigateTo` 或底部 `tab` 切换到其他页面，小程序切入后台等。
+     */
+    hide(this: This): void;
+    /** 页面生命周期回调—监听页面尺寸变化
+     *
+     * 所在页面尺寸变化时执行
+     */
+    resize(this: This, size: IMippWePage.IResizeOption): void;
+  }
+
+  /**
+   * Page生命周期方法:
+   *
+   * onShow(): void
+   *
+   * onHide(): void
+   *
+   * onResize(options: IMippWePage.IResizeOption): void
+   */
+  type IPageLifetime<This, IMethods> = Partial<IPageLifetimes<This & IMethodsOption<This, IMethods>>>
 
   /**
    * Component中options类型声明
@@ -31,5 +73,13 @@ declare namespace IMippWeComponent {
    */
   interface IProperty extends WechatMiniprogram.Component.PropertyOption {}
 
-  interface ITriggerEventOption extends WechatMiniprogram.Component.TriggerEventOption {}
+  /**
+   * 组件methods声明
+   */
+  type Keys<IMethods> = keyof IMethods;
+
+  type IMethodsOption<This, IMethods> = { [K in Keys<IMethods>]: (this: (IMethods extends any | null | undefined ? This : This & IMethods), ...args: any[]) => any;};
+
+  interface ITriggerEventOption
+    extends WechatMiniprogram.Component.TriggerEventOption {}
 }
