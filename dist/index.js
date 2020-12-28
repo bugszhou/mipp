69b68ee58bb1cc1b9cba63cdfa1ee1270aaf7110
youzhao.zhou <youzhao.zhou@verystar.cn>
2020-12-28 14:56:54 +0800

    feat: 增加typings

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
 import Base from "../dist/Page/Base";
-import Component from "../dist/Componnet/Base";
+import Component from "../dist/Component/Base";
 export var PageBase = Base;
 export var ComponentBase = Component;
 export default {
