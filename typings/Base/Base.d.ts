e87187d3dd17ea720072f182739cdd710f69bcef
youzhao.zhou <youzhao.zhou@verystar.cn>
2021-01-07 22:02:16 +0800

    feat: 增加Base

---
+++ typings/Base/Base.d.ts
@@ -0,0 +1,130 @@
+type DataOption = Record<string, any>;
+
+export declare class Base<IData extends DataOption> {
+  /** `setData` 函数用于将数据从逻辑层发送到视图层
+   *（异步），同时改变对应的 `this.data` 的值（同步）。
+   *
+   * **注意：**
+   *
+   * 1. **直接修改 this.data 而不调用 this.setData 是无法改变页面的状态的，还会造成数据不一致**。
+   * 2. 仅支持设置可 JSON 化的数据。
+   * 3. 单次设置的数据不能超过1024kB，请尽量避免一次设置过多的数据。
+   * 4. 请不要把 data 中任何一项的 value 设为 `undefined` ，否则这一项将不被设置并可能遗留一些潜在问题。
+   */
+  setData(
+    /** 这次要改变的数据
+     *
+     * 以 `key: value` 的形式表示，将 `this.data` 中的 `key` 对应的值改变成 `value`。
+     *
+     * 其中 `key` 可以以数据路径的形式给出，支持改变数组中的某一项或对象的某个属性，如 `array[2].message`，`a.b.c.d`，并且不需要在 this.data 中预先定义。
+     */
+    data: Partial<IData> & WechatMiniprogram.IAnyObject,
+    /** setData引起的界面更新渲染完毕后的回调函数，最低基础库： `1.5.0` */
+    callback?: () => void
+  ): void;
+
+  /** 检查组件是否具有 `behavior` （检查时会递归检查被直接或间接引入的所有behavior） */
+  hasBehavior(behavior: object): void;
+
+  /** 触发事件，参见组件事件 */
+  triggerEvent(
+    name: string,
+    detail?: object,
+    options?: WechatMiniprogram.Component.TriggerEventOption
+  ): void;
+
+  /** 创建一个 SelectorQuery 对象，选择器选取范围为这个组件实例内 */
+  createSelectorQuery(): WechatMiniprogram.SelectorQuery;
+
+  /** 创建一个 IntersectionObserver 对象，选择器选取范围为这个组件实例内 */
+  createIntersectionObserver(
+    options: WechatMiniprogram.CreateIntersectionObserverOption
+  ): WechatMiniprogram.IntersectionObserver;
+
+  /** 使用选择器选择组件实例节点，返回匹配到的第一个组件实例对象（会被 `wx://component-export` 影响） */
+  selectComponent(
+    selector: string
+  ): WechatMiniprogram.Component.TrivialInstance;
+
+  /** 使用选择器选择组件实例节点，返回匹配到的全部组件实例对象组成的数组 */
+  selectAllComponents(
+    selector: string
+  ): WechatMiniprogram.Component.TrivialInstance[];
+
+  /**
+   * 选取当前组件节点所在的组件实例（即组件的引用者），返回它的组件实例对象（会被 `wx://component-export` 影响）
+   *
+   * 最低基础库版本：[`2.8.2`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  selectOwnerComponent(): WechatMiniprogram.Component.TrivialInstance;
+
+  /** 获取这个关系所对应的所有关联节点，参见 组件间关系 */
+  getRelationNodes(
+    relationKey: string
+  ): WechatMiniprogram.Component.TrivialInstance[];
+
+  /**
+   * 立刻执行 callback ，其中的多个 setData 之间不会触发界面绘制（只有某些特殊场景中需要，如用于在不同组件同时 setData 时进行界面绘制同步）
+   *
+   * 最低基础库版本：[`2.4.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  groupSetData(callback?: () => void): void;
+
+  /**
+   * 返回当前页面的 custom-tab-bar 的组件实例
+   *
+   * 最低基础库版本：[`2.6.2`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  getTabBar(): WechatMiniprogram.Component.TrivialInstance;
+
+  /**
+   * 返回页面标识符（一个字符串），可以用来判断几个自定义组件实例是不是在同一个页面内
+   *
+   * 最低基础库版本：[`2.7.1`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  getPageId(): string;
+
+  /**
+   * 执行关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
+   *
+   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  animate(
+    selector: string,
+    keyFrames: WechatMiniprogram.Component.KeyFrame[],
+    duration: number,
+    callback: () => void
+  ): void;
+
+  /**
+   * 执行关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
+   *
+   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  animate(
+    selector: string,
+    keyFrames: WechatMiniprogram.Component.ScrollTimelineKeyframe[],
+    duration: number,
+    scrollTimeline: WechatMiniprogram.Component.ScrollTimelineOption
+  ): void;
+
+  /**
+   * 清除关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
+   *
+   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  clearAnimation(selector: string, callback: () => void): void;
+
+  /**
+   * 清除关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
+   *
+   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
+   **/
+  clearAnimation(
+    selector: string,
+    options: WechatMiniprogram.Component.ClearAnimationOptions,
+    callback: () => void
+  ): void;
+
+  getOpenerEventChannel(): WechatMiniprogram.EventChannel;
+}
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,138 @@
 /// <reference path="./weapp/index.d.ts" />
-/// <reference path="./Base/PageBase.d.ts" />
 import "weapp-api-typings";
+import { Base } from "./Base/Base";
 
-declare class ComponentBase<IData extends Record<string, any>> {
+declare class PageBase<IData extends Record<string, any>> extends Base<IData> {
+  constructor(options?: any);
+  get componentName(): string;
+
+  /** 页面的初始数据
+   *
+   * `data` 是页面第一次渲染使用的**初始数据**。
+   *
+   * 页面加载时，`data` 将会以`JSON`字符串的形式由逻辑层传至渲染层，因此`data`中的数据必须是可以转成`JSON`的类型：字符串，数字，布尔值，对象，数组。
+   *
+   * 渲染层可以通过 `WXML` 对数据进行绑定。
+   */
+  data: IData;
+
+  /** 页面的文件路径 */
+  is: string;
+
+  /** 到当前页面的路径 */
+  route: string;
+
+  /** 打开当前页面路径中的参数 */
+  options: Record<string, string | undefined>;
+
+  /** 生命周期回调—监听页面加载
+   *
+   * 页面加载时触发。一个页面只会调用一次，可以在 onLoad 的参数中获取打开当前页面路径中的参数。
+   */
+  onLoad(
+    /** 打开当前页面路径中的参数 */
+    query: Record<string, string | undefined>
+  ): void | Promise<void>;
+
+  /** 生命周期回调—监听页面显示
+   *
+   * 页面显示/切入前台时触发。
+   */
+  onShow(): void | Promise<void>;
+
+  /** 生命周期回调—监听页面初次渲染完成
+   *
+   * 页面初次渲染完成时触发。一个页面只会调用一次，代表页面已经准备妥当，可以和视图层进行交互。
+   *
+
+  * 注意：对界面内容进行设置的 API 如`wx.setNavigationBarTitle`，请在`onReady`之后进行。
+  */
+  onReady(): void | Promise<void>;
+
+  /** 生命周期回调—监听页面隐藏
+   *
+   * 页面隐藏/切入后台时触发。 如 `navigateTo` 或底部 `tab` 切换到其他页面，小程序切入后台等。
+   */
+  onHide(): void | Promise<void>;
+
+  /** 生命周期回调—监听页面卸载
+   *
+   * 页面卸载时触发。如`redirectTo`或`navigateBack`到其他页面时。
+   */
+  onUnload(): void | Promise<void>;
+
+  /** 监听用户下拉动作
+   *
+   * 监听用户下拉刷新事件。
+   * - 需要在`app.json`的`window`选项中或页面配置中开启`enablePullDownRefresh`。
+   * - 可以通过`wx.startPullDownRefresh`触发下拉刷新，调用后触发下拉刷新动画，效果与用户手动下拉刷新一致。
+   * - 当处理完数据刷新后，`wx.stopPullDownRefresh`可以停止当前页面的下拉刷新。
+   */
+  onPullDownRefresh(): void | Promise<void>;
+
+  /** 页面上拉触底事件的处理函数
+   *
+   * 监听用户上拉触底事件。
+   * - 可以在`app.json`的`window`选项中或页面配置中设置触发距离`onReachBottomDistance`。
+   * - 在触发距离内滑动期间，本事件只会被触发一次。
+   */
+  onReachBottom(): void | Promise<void>;
+
+  /** 用户点击右上角转发
+   *
+   * 监听用户点击页面内转发按钮（`<button>` 组件 `open-type="share"`）或右上角菜单“转发”按钮的行为，并自定义转发内容。
+   *
+   * **注意：只有定义了此事件处理函数，右上角菜单才会显示“转发”按钮**
+   *
+   * 此事件需要 return 一个 Object，用于自定义转发内容
+   */
+  onShareAppMessage(
+    /** 分享发起来源参数 */
+    options: IMippWePage.IShareAppMessageOption
+  ): IMippWePage.ICustomShareContent | void;
+
+  /**
+   * 监听右上角菜单“分享到朋友圈”按钮的行为，并自定义分享内容
+   *
+   * 本接口为 Beta 版本，暂只在 Android 平台支持，详见 [分享到朋友圈 (Beta)](https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/share-timeline.html)
+   *
+   * 基础库 2.11.3 开始支持，低版本需做兼容处理。
+   */
+  onShareTimeline(): WechatMiniprogram.Page.ICustomTimelineContent | void;
+
+  /** 页面滚动触发事件的处理函数
+   *
+   * 监听用户滑动页面事件。
+   */
+  onPageScroll(
+    /** 页面滚动参数 */
+    options: IMippWePage.IPageScrollOption
+  ): void | Promise<void>;
+
+  /** 当前是 tab 页时，点击 tab 时触发，最低基础库： `1.9.0` */
+  onTabItemTap(
+    /** tab 点击参数 */
+    options: IMippWePage.ITabItemTapOption
+  ): void | Promise<void>;
+
+  /** 窗口尺寸改变时触发，最低基础库：`2.4.0` */
+  onResize(
+    /** 窗口尺寸参数 */
+    options: IMippWePage.IResizeOption
+  ): void | Promise<void>;
+
+  /**
+   * 监听用户点击右上角菜单“收藏”按钮的行为，并自定义收藏内容。
+   * 基础库 2.10.3，安卓 7.0.15 版本起支持，iOS 暂不支持
+   */
+  onAddToFavorites(
+    options: IMippWePage.IAddToFavoritesOption
+  ): IMippWePage.IAddToFavoritesContent;
+}
+
+declare class ComponentBase<
+  IData extends Record<string, any>
+> extends Base<IData> {
   constructor(options?: any);
 
   /**
@@ -27,131 +157,4 @@ declare class ComponentBase<IData extends Record<string, any>> {
 
   /** 节点dataset */
   dataset: Record<string, string>;
-
-  /** `setData` 函数用于将数据从逻辑层发送到视图层
-   *（异步），同时改变对应的 `this.data` 的值（同步）。
-   *
-   * **注意：**
-   *
-   * 1. **直接修改 this.data 而不调用 this.setData 是无法改变页面的状态的，还会造成数据不一致**。
-   * 1. 仅支持设置可 JSON 化的数据。
-   * 1. 单次设置的数据不能超过1024kB，请尽量避免一次设置过多的数据。
-   * 1. 请不要把 data 中任何一项的 value 设为 `undefined` ，否则这一项将不被设置并可能遗留一些潜在问题。
-   */
-  setData(
-    /** 这次要改变的数据
-     *
-     * 以 `key: value` 的形式表示，将 `this.data` 中的 `key` 对应的值改变成 `value`。
-     *
-     * 其中 `key` 可以以数据路径的形式给出，支持改变数组中的某一项或对象的某个属性，如 `array[2].message`，`a.b.c.d`，并且不需要在 this.data 中预先定义。
-     */
-    data: Partial<IData> & WechatMiniprogram.IAnyObject,
-    /** setData引起的界面更新渲染完毕后的回调函数，最低基础库： `1.5.0` */
-    callback?: () => void
-  ): void;
-
-  /** 检查组件是否具有 `behavior` （检查时会递归检查被直接或间接引入的所有behavior） */
-  hasBehavior(behavior: object): void;
-
-  /** 触发事件，参见组件事件 */
-  triggerEvent(
-    name: string,
-    detail?: object,
-    options?: WechatMiniprogram.Component.TriggerEventOption
-  ): void;
-
-  /** 创建一个 SelectorQuery 对象，选择器选取范围为这个组件实例内 */
-  createSelectorQuery(): WechatMiniprogram.SelectorQuery;
-
-  /** 创建一个 IntersectionObserver 对象，选择器选取范围为这个组件实例内 */
-  createIntersectionObserver(
-    options: WechatMiniprogram.CreateIntersectionObserverOption
-  ): WechatMiniprogram.IntersectionObserver;
-
-  /** 使用选择器选择组件实例节点，返回匹配到的第一个组件实例对象（会被 `wx://component-export` 影响） */
-  selectComponent(
-    selector: string
-  ): WechatMiniprogram.Component.TrivialInstance;
-
-  /** 使用选择器选择组件实例节点，返回匹配到的全部组件实例对象组成的数组 */
-  selectAllComponents(
-    selector: string
-  ): WechatMiniprogram.Component.TrivialInstance[];
-
-  /**
-   * 选取当前组件节点所在的组件实例（即组件的引用者），返回它的组件实例对象（会被 `wx://component-export` 影响）
-   *
-   * 最低基础库版本：[`2.8.2`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  selectOwnerComponent(): WechatMiniprogram.Component.TrivialInstance;
-
-  /** 获取这个关系所对应的所有关联节点，参见 组件间关系 */
-  getRelationNodes(
-    relationKey: string
-  ): WechatMiniprogram.Component.TrivialInstance[];
-
-  /**
-   * 立刻执行 callback ，其中的多个 setData 之间不会触发界面绘制（只有某些特殊场景中需要，如用于在不同组件同时 setData 时进行界面绘制同步）
-   *
-   * 最低基础库版本：[`2.4.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  groupSetData(callback?: () => void): void;
-
-  /**
-   * 返回当前页面的 custom-tab-bar 的组件实例
-   *
-   * 最低基础库版本：[`2.6.2`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  getTabBar(): WechatMiniprogram.Component.TrivialInstance;
-
-  /**
-   * 返回页面标识符（一个字符串），可以用来判断几个自定义组件实例是不是在同一个页面内
-   *
-   * 最低基础库版本：[`2.7.1`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  getPageId(): string;
-
-  /**
-   * 执行关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
-   *
-   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  animate(
-    selector: string,
-    keyFrames: WechatMiniprogram.Component.KeyFrame[],
-    duration: number,
-    callback: () => void
-  ): void;
-
-  /**
-   * 执行关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
-   *
-   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  animate(
-    selector: string,
-    keyFrames: WechatMiniprogram.Component.ScrollTimelineKeyframe[],
-    duration: number,
-    scrollTimeline: WechatMiniprogram.Component.ScrollTimelineOption
-  ): void;
-
-  /**
-   * 清除关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
-   *
-   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  clearAnimation(selector: string, callback: () => void): void;
-
-  /**
-   * 清除关键帧动画，详见[动画](https://developers.weixin.qq.com/miniprogram/dev/framework/view/animation.html)
-   *
-   * 最低基础库版本：[`2.9.0`](https://developers.weixin.qq.com/miniprogram/dev/framework/compatibility.html)
-   **/
-  clearAnimation(
-    selector: string,
-    options: WechatMiniprogram.Component.ClearAnimationOptions,
-    callback: () => void
-  ): void;
-
-  getOpenerEventChannel(): WechatMiniprogram.EventChannel;
 }
