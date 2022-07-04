f12f635b109555371dfdc297405e3bd6705b0877
youzhao.zhou <youzhao.zhou@verystar.cn>
2022-07-04 14:10:15 +0800

    feat: 增加泛型插槽

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -136,17 +136,17 @@ export class PageBase<IData extends Record<string, any>> extends Base<IData> {
   ): IMippWePage.IAddToFavoritesContent;
 }
 
-export interface IMiniEvent<IDetail = unknown> {
+export interface IMiniEvent<IDetail = unknown, ITarget = any, ICurrentTarget = any> {
   /** 事件类型 */
   type: string;
   /** 页面打开到触发事件所经过的毫秒数 */
   timeStamp: number;
   detail?: IDetail;
   target?: {
-    dataset: Record<string, unknown>;
+    dataset: Record<string, ITarget>;
   };
   currentTarget?: {
-    dataset: Record<string, unknown>;
+    dataset: Record<string, ICurrentTarget>;
   };
 }
 
