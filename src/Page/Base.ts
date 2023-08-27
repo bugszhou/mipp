1af119c5ec7d446c71635a8422ef45869fd94469
youzhao.zhou <youzhao.zhou@verystar.cn>
2023-08-27 12:34:22 +0800

    feat: 增加异步的onReadyAsync

--- src/Page/Base.ts
+++ src/Page/Base.ts
@@ -80,9 +80,10 @@ export default class Base<IData = any> {
       } catch {}
 
       let isError = false;
+      let beforeResult: any = null;
 
       try {
-        beforeObj?.onReady?.apply(this, opts);
+        beforeResult = beforeObj?.onReady?.apply(this, opts);
       } catch (e) {
         console.error(e);
         isError = true;
@@ -94,7 +95,19 @@ export default class Base<IData = any> {
 
       isError = false;
 
-      return readyFn?.apply?.(this, opts);
+      let readyResult = readyFn?.apply?.(this, opts);
+
+      if (
+        typeof beforeResult === "object" &&
+        typeof beforeResult?.then === "function"
+      ) {
+        (async () => {
+          await beforeResult;
+          return that?.onReadyAsync?.apply?.(this, opts);
+        })();
+      }
+
+      return readyResult;
     };
 
     const showFn = that?.onShow;
